d524c056519db0cbc4da1ee8ab01d794f46cac9c
sphere <sphere@dersphere.de>
2011-03-12 11:02:14 +0000

    [BigPictures] Cosmetic: Respect more pep8 (expect line length)
    git-svn-id: http://sphere-xbmc-addons.googlecode.com/svn/trunk/script.image.bigpictures@99 cf22213b-6635-c232-c6a0-04a44ba58b2f

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -9,15 +9,15 @@ import sbb_scraper
 import wsj_scraper
 
 Addon = sys.modules['__main__'].Addon
-#enable localization
+# enable localization
 getLS = Addon.getLocalizedString
 
 
 class GUI(xbmcgui.WindowXML):
-    #Label Controls
+    # Label Controls
     CONTROL_MAIN_IMAGE = 100
     CONTROL_USAGE_TEXT = 103
-    #Label Actions
+    # Label Actions
     ACTION_CONTEXT_MENU = [117]
     ACTION_MENU = [122]
     ACTION_PREVIOUS_MENU = [9]
@@ -27,7 +27,7 @@ class GUI(xbmcgui.WindowXML):
     ACTION_UP = [3]
 
     ACTIVESOURCE = 0
-    
+
     SOURCES = list()
     SOURCES.append({'name': 'Boston.com: The Big Picture', 'object': 'tbp', 'url': 'http://www.boston.com/bigpicture/'})
     SOURCES.append({'name': 'Boston.com: The Big Shot', 'object': 'tbp', 'url': 'http://www.boston.com/sports/blogs/bigshots/'})
@@ -42,7 +42,7 @@ class GUI(xbmcgui.WindowXML):
 
     def onInit(self):
         self.showInfo = 'true'
-        self.getControl(102).setLabel(getLS(32001)) #fixme
+        self.getControl(102).setLabel(getLS(32001))  # fixme
         self.showAlbums()
 
     def onFocus(self, controlId):
@@ -104,15 +104,15 @@ class GUI(xbmcgui.WindowXML):
         downloadPath = xbmcgui.Dialog().browse(3, ' '.join([getLS(32020), getLS(32022)]), 'pictures')
         if downloadPath:
             if self.getProperty('type') == 'photo':
-                photos = [{'pic':self.getProperty('pic'), 'title': ''}] #url needs to be passed as a dict in a list.
-                imageDownloader.Download(photos, downloadPath) 
+                photos = [{'pic':self.getProperty('pic'), 'title': ''}]  # url needs to be passed as a dict in a list.
+                imageDownloader.Download(photos, downloadPath)
             elif self.getProperty('type') == 'album':
-                pDialog = xbmcgui.DialogProgress() #show useless dialog so user knows something is happening.
+                pDialog = xbmcgui.DialogProgress()  # show useless dialog so user knows something is happening.
                 pDialog.create(self.SOURCES[self.ACTIVESOURCE]['name'])
                 link = self.getProperty('link')
                 pDialog.update(50)
                 if self.SOURCES[self.ACTIVESOURCE]['object'] == 'tbp':
-                    self.tbp.getPhotos(link) # Get a list of photos from the link.
+                    self.tbp.getPhotos(link)  # Get a list of photos from the link.
                     photos = self.tbp.photos
                 elif self.SOURCES[self.ACTIVESOURCE]['object'] == 'sbb':
                     self.sbb.getPhotos(link)
@@ -126,12 +126,12 @@ class GUI(xbmcgui.WindowXML):
                 pDialog.close()
                 imageDownloader.Download(photos, downloadPath)
 
-    def showPhotos(self): #the order is significant!
+    def showPhotos(self):  # the order is significant!
         self.getControl(self.CONTROL_USAGE_TEXT).setText('\n'.join([getLS(32030), getLS(32031), getLS(32032)]))
         link = self.getProperty('link')
-        self.getControl(self.CONTROL_MAIN_IMAGE).reset() #Clear the old list of albums.
+        self.getControl(self.CONTROL_MAIN_IMAGE).reset()  # Clear the old list of albums.
         if self.SOURCES[self.ACTIVESOURCE]['object'] == 'tbp':
-            self.tbp.getPhotos(link) # Get a list of photos from the link.
+            self.tbp.getPhotos(link)  # Get a list of photos from the link.
             photos = self.tbp.photos
         elif self.SOURCES[self.ACTIVESOURCE]['object'] == 'sbb':
             self.sbb.getPhotos(link)
@@ -145,7 +145,7 @@ class GUI(xbmcgui.WindowXML):
 
     def showAlbums(self):
         self.getControl(self.CONTROL_USAGE_TEXT).setText('\n'.join([getLS(32040), getLS(32041), getLS(32042)]))
-        self.getControl(self.CONTROL_MAIN_IMAGE).reset() #This is necessary when returning from photos.
+        self.getControl(self.CONTROL_MAIN_IMAGE).reset()  # This is necessary when returning from photos.
         if self.SOURCES[self.ACTIVESOURCE]['object'] == 'tbp':
             self.tbp.getAlbums(self.SOURCES[self.ACTIVESOURCE]['url'])
             albums = self.tbp.albums
@@ -169,11 +169,11 @@ class GUI(xbmcgui.WindowXML):
             # title = Title of this Album or Picture: "Best Pictures of the Week"
             item['title'] = item['title']
             # duration =  act-element/len-elements: "1/12"
-            item['duration']= '%s/%s' % (i+1, total) #fixme: find something better then this
+            item['duration'] = '%s/%s' % (i + 1, total)  # fixme: find something better then this
             self.addListItem(self.CONTROL_MAIN_IMAGE, item)
 
     def addListItem(self, controlId, properties):
-        #print properties
+        # print properties
         li = xbmcgui.ListItem(label=properties['title'], label2=properties['description'], iconImage=properties['pic'])
         for p in properties.keys():
             li.setProperty(p, properties[p])
