06f2d39221db7ce1657232602a090282f7cb249b
sphere <sphere@dersphere.de>
2011-12-08 22:01:48 +0100

    added "stern.de Bilder des Tages" in rss scraper

--- resources/lib/scrapers/6_rss.py
+++ resources/lib/scrapers/6_rss.py
@@ -1,21 +1,25 @@
 from scraper import ScraperPlugin
-
+import re
 
 class Scraper(ScraperPlugin):
 
     NAME = 'RSS Sources'
+    
+    PIC_REGEX = re.compile('media:content|enclosure')
+    DESC_REGEX = re.compile('media:description|description')
 
     def getAlbums(self):
-        rss_list = ('http://www.guardian.co.uk/world/series/eyewitness/rss', )
+        rss_list = ('http://www.guardian.co.uk/world/series/eyewitness/rss', 
+                    'http://www.stern.de/feed/daily/')
         
         self.albums = list()
         for rss_source in rss_list:
             tree = self.getCachedTree(rss_source)
-            print repr(tree)
             title = self.cleanHTML(tree.find('title'))
             link = rss_source
             description = self.cleanHTML(tree.find('description'))
-            pic = tree.find('item').findAll('media:content', {'type': 'image/jpeg'})[-1]['url']
+            element_regex = re.compile('media:content|enclosure')
+            pic = tree.find('item').findAll(self.PIC_REGEX, {'type': 'image/jpeg'})[-1]['url']
             self.albums.append({'title': title,
                                 'pic': pic,
                                 'description': description,
@@ -30,8 +34,8 @@ class Scraper(ScraperPlugin):
         for node in photoNodes:
             item_title = node.find('title')
             title = item_title
-            pic = node.findAll('media:content', {'type': 'image/jpeg'})[-1]['url']
-            description = node.find('media:description').string
+            pic = node.findAll(self.PIC_REGEX, {'type': 'image/jpeg'})[-1]['url']
+            description = node.find(self.DESC_REGEX).string
             self.photos.append({'title': self.cleanHTML(title),
                                 'pic': pic,
                                 'description': description})
